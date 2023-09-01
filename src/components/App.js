0c1d2c3a8e93f472b38332cd0e6daf8c03795a37
Aditya Kumar Pandey <114282369+adityapandey1111@users.noreply.github.com>
2023-09-02 02:18:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,40 @@
-
-import React ,{useState} from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
- 
-  const[input,setInput]=useState('');
-  const[tasks,setTasks]=useState([]);
+  const [input, setInput] = useState('');
+  const [tasks, setTasks] = useState([]);
 
-  console.log('input: ',input);
-  console.log('tasks: ',tasks);
-  function add(){
-    setTasks([...tasks,input]);
+  // Function to add a task to the list
+  function add() {
+    if (input.trim() !== '') { // Check if input is not empty
+      setTasks([...tasks, input]);
+      setInput(''); // Clear the input field
+    }
   }
 
-  function deleteTask(task){
-     setTasks(tasks.filter((t)=>{
-          if(t!==task) return t;
-     }))
+  // Function to delete a task from the list
+  function deleteTask(task) {
+    setTasks(tasks.filter(t => t !== task));
   }
-
 
   return (
-    <div style={{textAlign:'center'}}>
-        {/* Do not remove the main div */}
-        <p>To-Do List</p>
-         <div>
-          <input type='text'  value={input} onChange={(e)=>setInput(e.target.value)}/>
-          <button onClick={add}>Add Todo</button>
-         </div>
-         <ul>
-           {tasks.length>0 && tasks.map((task,index)=>{
-            return <li style={{display:'flex',justifyContent:'space-between'}} key={index}>
-             <span>{task}</span> 
-             <button onClick={()=>deleteTask(task)}>Delete</button>
-            </li>
-           })}
-         </ul>
+    <div style={{ textAlign: 'center' }}>
+      <p>To-Do List</p>
+      <div>
+        <input type='text' value={input} onChange={(e) => setInput(e.target.value)} />
+        <button onClick={add}>Add Todo</button>
+      </div>
+      <ul>
+        {tasks.length > 0 && tasks.map((task, index) => (
+          <li style={{ display: 'flex', justifyContent: 'space-between' }} key={index}>
+            <span>{task}</span>
+            <button onClick={() => deleteTask(task)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
